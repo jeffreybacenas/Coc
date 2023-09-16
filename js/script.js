a38184f7bf8c1ea4cfb672dc2ba64f11a7e92c3c
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-16 20:58:27 +0800

    last touch

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const sr = ScrollReveal ({
     duration: 2600,
     delay: 450,
     reset: true
-})
+});
 
 sr.reveal('.hero-text', {delay: 200, origin: 'top'});
 sr.reveal('.hero-img', {delay: 450, origin: 'top'});
